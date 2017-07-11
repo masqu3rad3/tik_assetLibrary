12a17c4c57b7ebd7f3090d993ec1df1744381d91
Arda Kutlu <ardakutlu@gmail.com>
2017-07-11 16:21:28 +0200

    While Loop fixed

--- assetLibrary.py
+++ assetLibrary.py
@@ -1,3 +1,49 @@
+#####################################################################################################################
+## Asset Library - Python Script
+## Copyright (C) Arda Kutlu
+## Title: Asset Library
+## AUTHOR:	Arda Kutlu
+## e-mail: ardakutlu@gmail.com
+## Web: http://www.ardakutlu.com
+## VERSION:1.0
+## CREATION DATE: 11.07.2017
+## LAST MODIFIED DATE: 11.07.2017
+##
+## DESCRIPTION: Asset Library which will hold the frequently used product models.
+## INSTALL:
+## Copy all files into user/maya/scripts folder(not folder, only files)
+## Run these commands in python tab (or put them in a shelf:
+## import assetLibrary
+## assetLibrary.AssetLibraryUI().show()
+##
+## USAGE:
+
+## MAIN MENU:
+## - Top search filter can be used to filter assets.
+## - Use the Export Import button to import selected asset. This will copy all texture files under the
+                ## sourceimages/<assetname> directory of currently set project
+## - Use Refresh Button to manually refresh the library.
+## - Use Export button to put selected objects into the Library
+
+## RIGHT CLICK MENU:
+## - Right click on an asset opens the right click menu
+## - Show screenshot: Opens the screenshot of the item with the default imageviewer defined by the os.
+## - Show wireframe: Opens the wireframe of the item with the default imageviewer defined by the os.
+## - Import and copy textures: Same command with the import button
+## - Import Maya file: Only imports the ma file from the asset folder
+## - Import Obj file: Imports the Obj file from the asset folder
+## - Open File: Opens the Maya File THIS WILL FORCE TO OPEN THE FILE, ALL UNSAVED WORK WILL LOST ON CURRENT SCENE
+## - View As List / View As Icons : Toggles between Icon and List View
+## - Show Folder In Explorer: Opens the asset folder in windows explorer
+
+## SHORTCUTS:
+## - CTRL+i will import the file. Same action with the "import" button
+## - CTRL+e will export selection. Same action with the "export" button
+## - CTRL+PLUS(+) will increase the icon size
+## - CTRL+MINUS(-) will decrease the icon size
+
+#####################################################################################################################
+
 import pymel.core as pm
 import json
 import os, fnmatch
@@ -359,20 +405,9 @@ class assetLibrary(dict):
         except:
             # if there is no sg, return en empty list
             return []
-        objMaterial = pm.ls(pm.listConnections(sg), materials=True)
-        print "objMaterial", objMaterial
-        everyInput = []
-        nextInputs = objMaterial
-        # iterCount = 0
-        while nextInputs != []:
-            print "nextInputs", nextInputs
-            # iterCount += 1
-            everyInput += nextInputs
-            tempInputs = []
-            for i in nextInputs:
-                tempInputs += checkInputs(i)
-            nextInputs = tempInputs
-        everyInput = self.makeUnique(everyInput)
+
+        everyInput = self.gatherAllinputs(sg)
+
         fileNodes = pm.ls(everyInput, type="file")
         return fileNodes
 
@@ -381,6 +416,23 @@ class assetLibrary(dict):
         for e in fList:
             keys[e] = 1
         return keys.keys()
+
+    def gatherAllinputs(self, node):
+        allInputs = []
+        currentInputs = node.inputs()
+
+        while len(allInputs) != len(currentInputs):
+            print "allinputs", len(allInputs)
+            print "currentinputs", len(currentInputs)
+            tempInputs = []
+            for i in currentInputs:
+                tempInputs += i.inputs()
+
+            allInputs = currentInputs
+            currentInputs += tempInputs
+            currentInputs = self.makeUnique(currentInputs)
+            allInputs = self.makeUnique(allInputs)
+        return allInputs
 
     def pathOps(self, fullPath, mode):
         """
@@ -559,7 +611,7 @@ class AssetLibraryUI(QtWidgets.QDialog):
 
         self.popMenu.addSeparator()
 
-        openFolderAction = QtWidgets.QAction('Open folder in explorer', self)
+        openFolderAction = QtWidgets.QAction('Show folder in explorer', self)
         self.popMenu.addAction(openFolderAction)
         openFolderAction.triggered.connect(lambda item='openFolder': self.actionTrigger(item))
 
